1b70753e934748bb9fbfbfe1c2e201ffd06aa17c
Tolulope-Esther <tolulopeesther69@gmail.com>
2020-05-11 22:17:19 +0100

    new

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 var erase = document.querySelector("#erase");
-var user = parseFloat(document.querySelector("#user"));
-var num = document.querySelector(".num");
-var ops = document.querySelector(".ops");
+var user = document.querySelector("#user");
+var ops = document.getElementsByClassName("ops");
 var equals = document.querySelector("#equals");
-// var restart = document.querySelector("#restart");
-var theNum = ""; // to invkoke a function later both for current and later number
-var oldNum = "";
-var resultNum; // variable will be object later
+var num = document.getElementsByClassName("num");
+var period = document.getElementById("period");h 
+var currentNum = '0'; // to invkoke a function later both for current and later number
+var pendingNum;
+var resultNum = []; // array method to run the calculation 
 var operator;
 
+function stopPeriod() {
+  var str = '.';
+  document.getElementById("user").innerHTML += str.replace(1);
+}
 
-// Operator
+var addNum = function() {
+  oldNum = theNum;
+  theNum = "";
+  operator = getAttribute("unit-ops");
+}
+
+for (let i = 0; i < num.length; i++) {
+  let button = num[i];
+  
+  button.addEventListener("click", function() {
+    // Operators
+    // if (num === '.' && this.theNum.includes('.'))
+    // return;
+    // });
+
+
 if (ops == "plus") {
   resultNum = oldNum + theNum;
 } else if (ops == "minus") {
@@ -22,13 +41,21 @@ if (ops == "plus") {
 }
 resultNum = theNum; //if equal button is clicked without an operator
 // To display number when button is clicked
-
-var setNum = function () {
-  if (resultNum){
-    theNum = this.getAttribute("unit-num");
-    resultNum = "";
-  } else {
-    theNum += this.getAttribute("data-num");
-  }
-  user.innerHTML = theNum;
+  });
 }
+
+
+// var setNum = function () {
+//     if (resultNum){
+//       theNum = this.getAttribute("unit-num");
+//       resultNum = "";
+//     } else {
+//       theNum += this.getAttribute("data-num");
+//    }
+//    console.log(user.innerHTML = theNum);
+// }
+
+// function view(val){
+//   document.getElementById("user").value += val;
+// }
+// 
