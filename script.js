014f6e7d70960f4b13df8ce3e03d3375861774b8
Tolulope-Esther <tolulopeesther69@gmail.com>
2020-05-18 23:39:12 +0100

    corrections

--- script.js
+++ script.js
@@ -8,126 +8,146 @@ var period = document.getElementById("period");
 
 //Temporary variables
 var firstNum = '0'; // to invkoke a function later both for current and later number
-var secondNum;
+var secondNum = '0';
 var evalStringArray = []; // array method to run the calculation 
 var operator =null;
 
 // Erase function
 erase.onclick = () => {
   firstNum = '0';
-  secondNum = undefined;
+  secondNum = "0";
   evalStringArray = [];
-  user.innerHTML = currentNum;
+  user.innerHTML = firstNum;
   }
 
 // Event listener to operation and number buttons using for loop
 for (let i = 0; i<num.length; i++) {
-  num[i].addEventListener('click', updateNum)
+  num[i].addEventListener('click', toggleUpdate)
 }
 for (let i = 0; i<ops.length; i++) {
   ops[i].addEventListener('click', operation)
 }
-// Functions that will update the current number
-function updateCurrentNum (e) {
-  var numText = e.target.innerText; // Or I use getAttribute _unit-num
+//Functions that will update the current number
+
+
+function updatefirstNum (e) {
+  var numText = e.target.innerText;
   if (firstNum === "0") {
-    firstNum ="";
+    firstNum = "";
   }
-  // Append current number
+  //Append first number
   firstNum += numText;
 
- user.innerText = firstNum;
+  user.innerText = firstNum;
 }
-function updateNum (e) {
+function updatefirstNum (e) {
   var numText = e.target.innerText; // Or I use getAttribute _unit-num
   if (firstNum === "0" && numText !== ".") {
     firstNum ="";
   }
+  //check if firstNum already has .
  else if (numText === "." && firstNum.includes('.')) {
    numText = null;
  }
  else {
   firstNum += numText;
-  user.innerText = firstNum;
+ user.innerText = firstNum;
+ console.log(firstNum);
  }
-if (operator === null) {
-  console.log(firstNum)
 }
+
+function updatesecondNum (e) {
+  var numText = e.target.innerText;
+  if (secondNum === "0") {
+    secondNum = "";
+  }
+  //Append first number
+  secondNum += numText;
+
+  user.innerText = secondNum;
+} 
+function updatesecondNum (e) {
+  var numText = e.target.innerText; // Or I use getAttribute _unit-num
+  if (secondNum === "0" && numText !== ".") {
+    secondNum ="";
+  }
+  //check if secondNum already has .
+ else if (numText === "." && firstNum.includes('.')) {
+   numText = null;
+ }
+ else {
+  secondNum += numText;
+ user.innerText = secondNum;
+ console.log(secondNum);
+ }
 }
+function toggleUpdate () {
+  if(operator === null) {
+  console.log(updatefirstNum);
+  } else updatesecondNum;
+}
+numText.click (toggleUpdate);
+
 
 //Operator function
 function operation (e) {
-  var operator = e.target.innerText;
-  if (operator === null){
-   console.log(firstNum);
-  }
-  if (operator != null) {
-    secondNum;
-  }
-// if(operator === "+") {
-//   pendingNum = currentNum;
-//   currentNum = "0";
-//   user.innerText = currentNum;
-// } else if (operator === "-") {
-//   pendingNum = currentNum;
-//   currentNum = "0";
-//   user.innerText = currentNum;
-// } else if (operator ==="*"){
-//   pendingNum = currentNum;
-//   currentNum = "0";
-//   user.innerText = currentNum;
-// } else if (operator === "/") {
-//   pendingNum = currentNum;
-//   currentNum = "0";
-//   user.innerText = currentNum;
-// }
+ var operator = e.target.innerText;
+if(operator === "+") {
+  secondNum = firstNum;
+  firstNum = "0";
+  user.innerText = firstNum;
+} else if (operator === "-") {
+  secondNum = firstNum;
+  firstNum = "0";
+  user.innerText = firstNum;
+} else if (operator ==="*"){
+  secondNum = firstNum;
+  firstNum = "0";
+  user.innerText = firstNum;
+} else if (operator === "/") {
+  secondNum = firstNum;
+  firstNum = "0";
+  user.innerText = firstNum;
 }
-
-//Equal functiom
-  equals.addEventListener('click', calculate);
-
-  function calculate () {
-  
-  }
+}
 
 //Operator functions
-/*function operation (e) {
- var operator = e.target.innerText;
-  switch (operator) {
-    case '+':
-      pendingNum = currentNum;
-      currentNum ='0';
-      user.innerText = currentNum;
-      evalStringArray.push(pendingNum);
-      evalStringArray.push('+');
-        break;
-    case '-':
-      pendingNum = currentNum;
-      currentNum = '0';
-      user.innerText = currentNum;
-      evalStringArray.push(pendingNum);
-      evalStringArray.push('-');
-        break;
-    case '*':
-      pendingNum = currentNum;
-      currentNum = '0';
-      user.innerText = currentNum;
-      evalStringArray.push(pendingNum);
-      evalStringArray.push('*');
-        break;
-    case '/':
-      pendingNum = currentNum;
-      currentNum = '0';
-      user.innerText = currentNum;
-      evalStringArray.push(pendingNum);
-      evalStringArray.push('/');
-        break;
-    case '=':
-      evalStringArray.push(currentNum);
-      var evaluation = eval(evalStringArray.join(''));
-      currentNum = evaluation + '';
-      user.innerText = currentNum;
-      evalStringArray =[];
-        break;
-}
-}*/
+// function operation (e)
+//  var operator = e.target.innerText;
+//   switch (operator) {
+//     case '+':
+//       pendingNum = currentNum;
+//       currentNum ='0';
+//       user.innerText = currentNum;
+//       evalStringArray.push(pendingNum);
+//       evalStringArray.push('+');
+//         break;
+//     case '-':
+//       pendingNum = currentNum;
+//       currentNum = '0';
+//       user.innerText = currentNum;
+//       evalStringArray.push(pendingNum);
+//       evalStringArray.push('-');
+//         break;
+//     case '*':
+//       pendingNum = currentNum;
+//       currentNum = '0';
+//       user.innerText = currentNum;
+//       evalStringArray.push(pendingNum);
+//       evalStringArray.push('*');
+//         break;
+//     case '/':
+//       pendingNum = currentNum;
+//       currentNum = '0';
+//       user.innerText = currentNum;
+//       evalStringArray.push(pendingNum);
+//       evalStringArray.push('/');
+//         break;
+//     case '=':
+//       evalStringArray.push(currentNum);
+//       var evaluation = eval(evalStringArray.join(''));
+//       currentNum = evaluation + '';
+//       user.innerText = currentNum;
+//       evalStringArray =[];
+//         break;
+//   }
